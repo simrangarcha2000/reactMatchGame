c1785a1d0235657e1fbd6f4ea45011a47f76a266
Hee Jung Lee <jessicaheejung.lee@gmail.com>
2021-03-31 19:50:55 -0400

    added mobile header AD part

--- src/components/FormDesktopSide.js
+++ src/components/FormDesktopSide.js
@@ -1,12 +1,12 @@
-import wADimg1 from '../images/moneyBagTextHJ.png';
-import wADimg2 from '../images/moneyBagHJ.png';
+import dADimg1 from '../images/moneyBagTextHJ.png';
+import dADimg2 from '../images/moneyBagHJ.png';
 
 function FormDesktopSide(){
     return(
         <aside id="formDesktopAd">
         <div class="formDesktopAdImages">
-        <img src={wADimg1} alt="Chance to win $10,000" id="moneyBagText"/>
-        <img src={wADimg2} alt="Money bag" id="moneyBag"/>
+        <img src={dADimg1} alt="Chance to win $10,000" id="moneyBagText"/>
+        <img src={dADimg2} alt="Money bag" id="moneyBag"/>
         </div>
         <p class="whiteText">Match the right tiles of this game every 48-hour periods, If you win, you will get amazing prizes!</p>
         <p><u>The prizes are as follows: </u></p>
