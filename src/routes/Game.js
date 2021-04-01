6be70d26512be17ef94bc26c3cafbc8b2fd6c920
Alejandro Alvarez Porras <alejandro.alvarezporras@dcmail.ca>
2021-04-01 19:12:15 -0400

    sponsors added

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -6,6 +6,14 @@ const Game = () => {
     return (
         <main>
             <RegularHeader></RegularHeader>
+            <section>
+                <div className="titleInfo">
+                    <h1>GOOD LUCK</h1>
+                    <h3>First Name, Last Name</h3>
+                </div>
+                <div className="gameInfo"></div>
+                <div className="gameBoard"></div>
+            </section>
             <Footer></Footer>
         </main>
     );
