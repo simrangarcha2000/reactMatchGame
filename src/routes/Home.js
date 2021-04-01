3ef4088ec927ba6c2dd81e48f3598b9d11270cf7
Alejandro Alvarez Porras <alejandro.alvarezporras@dcmail.ca>
2021-04-01 19:08:23 -0400

    home components and added to route

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer";
+import RegularHeader from "../components/RegularHeader";
 
 const Home = () => {
-    return <main></main>;
+    return (
+        <main>
+            <RegularHeader></RegularHeader>
+            <Footer></Footer>
+        </main>
+    );
 };
 
 export default Home;
