ddb70adb2f153c2f827719c005f7a53c1ef3abb4
Alejandro Alvarez Porras <alejandro.alvarezporras@dcmail.ca>
2021-04-01 21:49:31 -0400

    homepage finished

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,10 +3,39 @@ import Footer from "../components/Footer";
 import RegularHeader from "../components/RegularHeader";
 import Sponsors from "../components/Sponsors";
 
+import cardsImage from "../images/homeCards.png";
+
 const Home = () => {
     return (
         <main>
             <RegularHeader></RegularHeader>
+            <section className="homeSection">
+                <div className="titleInfo home">
+                    <h1>WELCOME BACK</h1>
+                    <h3>First Name, Last Name</h3>
+                </div>
+                <div className="status">
+                    <img src={cardsImage} alt="" />
+                    <button className="playbtn">23:59:55</button>
+                    <div className="textStatus">
+                        <p className="text1">
+                            Unfortunately , you are not eligible to play the
+                            game now. Match the right tiles of this game every
+                            48-hour periods, If you win, you will get amazing
+                            prizes!
+                        </p>
+                        <p className="text2">
+                            The prizes are as follows: <br></br> 1 prize of
+                            $10,000 BuyMore Dollars, 5 prizes of $750 BuyMore
+                            Dollars, 10 prizes of $100 BuyMore Dollars, 100
+                            prizes of $20 BuyMore Dollars
+                        </p>
+                        <p className="text3">
+                            Come back soon to enjoy the game!!!
+                        </p>
+                    </div>
+                </div>
+            </section>
             <Sponsors></Sponsors>
             <Footer></Footer>
         </main>
