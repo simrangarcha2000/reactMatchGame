ca9f44f289148377c6829847d359564e924b982e
AMRITPAL SINGH <saffi1999@icloud.com>
2021-04-06 19:42:35 -0400

    added css styles

--- src/routes/WinPage.js
+++ src/routes/WinPage.js
@@ -16,6 +16,24 @@ function WinPage() {
                     <h1>WELCOME BACK</h1>
                     <h3>{form.username}</h3>
                 </div>
+
+                <div class="heading">
+                    <h1>Congrats</h1>
+                </div>
+                <div class="win-para">
+                    <p>You have WON the game.!!<br></br>Please answer the following question</p>
+                </div>
+                <div class="question-box">
+                    <p>What is the answer to 1+1? </p>
+                </div>
+                <div class="answer-box">
+                    <div class="grey-box"></div>
+                    <div class="btn-answer">
+                    <button class="btn-a">
+                        <p>ENTER</p>
+                    </button>
+                    </div>
+                </div>
                 </div>
             </section>
             <Sponsors></Sponsors>
