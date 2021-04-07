f446b74cfa42dd0b3404b5db6b5a67c418e3c754
AMRITPAL SINGH <saffi1999@icloud.com>
2021-04-06 21:10:35 -0400

    added coupen in loose page

--- src/routes/LosePage.js
+++ src/routes/LosePage.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import RegularHeader from "../components/RegularHeader";
 import Sponsors from "../components/Sponsors";
 import { useSelector } from "react-redux";
+import coupen from  "../images/coupen.png"
 
 function LosePage() {
 
@@ -23,10 +24,16 @@ function LosePage() {
             <div class="heading2">
                     <h1>Sorry!</h1>
                 </div>
-                <div class="win-para">
+                <div class="lose-para">
                     <p>Sad to see you lose the game. 
                     <br></br>Wins are not easy, and we understand that<br></br>
                     Better luck next time.</p>
+                </div>
+
+                <div class="coupen-banner">
+
+                    <img src={coupen} alt="coupens"/>
+
                 </div>
 
             </div>
