8b5917d6e1abe1515528d82ceac60ae9bbb75995
AMRITPAL SINGH <saffi1999@icloud.com>
2021-04-06 20:41:00 -0400

    added css for mobile view for win

--- src/routes/WinPage.js
+++ src/routes/WinPage.js
@@ -11,14 +11,14 @@ function WinPage() {
         <div>
             <RegularHeader></RegularHeader>
             <section class="yellow-box">
-                <div class="">
+                
             <div className="titleInfo home">
                     <h1>WELCOME BACK</h1>
                     <h3>{form.username}</h3>
                 </div>
-
+ <div class="win-content">
                 <div class="heading">
-                    <h1>Congrats</h1>
+                    <h1>Congrats!</h1>
                 </div>
                 <div class="win-para">
                     <p>You have WON the game.!!<br></br>Please answer the following question</p>
@@ -27,14 +27,31 @@ function WinPage() {
                     <p>What is the answer to 1+1? </p>
                 </div>
                 <div class="answer-box">
-                    <div class="grey-box"></div>
+                    <div class="grey">
+                    <input  class="a-box"   type="text"/>
+                    </div>
+
                     <div class="btn-answer">
                     <button class="btn-a">
                         <p>ENTER</p>
                     </button>
                     </div>
+
+                    <div class="time-box">
+
+                        <div class="time-p">
+                            <p>COME BACK IN</p>
+                        </div>
+
+                        <div class="timeFrame">
+                            <p>36:59:33</p>
+                        </div>
+
+                    </div>
                 </div>
+
                 </div>
+
             </section>
             <Sponsors></Sponsors>
             <Footer></Footer>
